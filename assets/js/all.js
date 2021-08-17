883f2a56ca716968049839a3816e53eccb70d5af
Tung <sky250331@gmail.com>
2021-08-17 21:28:39 +0800

    Update 2021-08-17T13:28:36.290Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -4,4 +4,26 @@ $('.expand-more').click(function (e) {
   e.preventDefault();
   $(this).toggleClass('active'); // 請使用 toggleClass 完成此作業
 });
+ClassicEditor.create(document.querySelector('#editor')).then(function (editor) {
+  console.log(editor);
+})["catch"](function (error) {
+  console.error(error);
+});
+var replyBtn = document.querySelector('.reply-btn');
+var cancelBtn = document.querySelector('.cancel-btn');
+var authorReply = document.querySelector('.author-reply');
+var authorEdit = document.querySelector('.author-editor');
+var groupBtn = document.querySelector('.btn-group');
+var replySection = document.querySelector('.reply-section');
+replyBtn.addEventListener('click', function () {
+  groupBtn.classList.add('d-none');
+  authorReply.classList.add('d-none');
+  authorEdit.classList.add('d-block');
+  replySection.classList.add('changestyle');
+});
+cancelBtn.addEventListener('click', function () {
+  groupBtn.classList.remove('d-none');
+  authorReply.classList.remove('d-none');
+  authorEdit.classList.remove('d-block');
+});
 //# sourceMappingURL=all.js.map
